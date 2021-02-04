679169240966fc2d9ba9664cb1989bce70299435
Kannan Suresh <kannansuresh@outlook.com>
2021-02-04 10:05:12 +0530

    Added buyme coffee code.

--- docs/js/custom.js
+++ docs/js/custom.js
@@ -1,13 +1,13 @@
 setInterval(removePreloader, 10000);
 function removePreloader(params) {
-    $('.preloader').fadeOut(1000);
+  $(".preloader").fadeOut(1000);
 }
 
-$(window).ready(function () {    
-    $('.col-md').addClass('align-self-center');
-    if ($('#ytplayer').length) {
-        testYouTubeConnection();
-    }
+$(window).ready(function () {
+  $(".col-md").addClass("align-self-center");
+  if ($("#ytplayer").length) {
+    testYouTubeConnection();
+  }
 });
 
 // preloader
@@ -17,72 +17,71 @@ $(window).ready(function () {
 // });
 
 // preloader
-$(window).on('load', function () {
-    $('.preloader').fadeOut(1000); // set duration in brackets     
+$(window).on("load", function () {
+  $(".preloader").fadeOut(1000); // set duration in brackets
 });
 
-
-$('#blog').onclick = function () {
-    location.href = "https://www.aneejian.com";
+$("#blog").onclick = function () {
+  location.href = "https://www.aneejian.com";
 };
 
 $(function () {
-    new WOW().init();
-    $('.templatemo-nav').singlePageNav({
-        offset: 70
-    });
+  new WOW().init();
+  $(".templatemo-nav").singlePageNav({
+    offset: 70,
+  });
 
-    $('.head-nav').singlePageNav({
-        offset: 70
-    });
+  $(".head-nav").singlePageNav({
+    offset: 70,
+  });
 
-    /* Hide mobile menu after clicking on a link
+  /* Hide mobile menu after clicking on a link
     -----------------------------------------------*/
-    $('.navbar-collapse a').click(function () {
-        $(".navbar-collapse").collapse('hide');
-    });
+  $(".navbar-collapse a").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 });
 
 function changePage(event) {
-    if ($(event.target).hasClass('external')) {
-        window.location.href = $(event.target).attr('href');
-        return;
-    }
+  if ($(event.target).hasClass("external")) {
+    window.location.href = $(event.target).attr("href");
+    return;
+  }
 }
 
 $(function () {
-    $('.nav li').click(changePage);
+  $(".nav li").click(changePage);
 });
 
 $(function () {
-    $('.nav-link').click(changePage);
-    $('.navbar-brand').click(changePage);
+  $(".nav-link").click(changePage);
+  $(".navbar-brand").click(changePage);
 });
 
-
 function testYouTubeConnection() {
-    var playerFrame = $('#ytplayer');
-    var youTube = 'https://www.youtube.com/embed/KmaOW4PhuII?rel=0';
-    var youTubeNoCookie = 'https://www.youtube-nocookie.com/embed/KmaOW4PhuII?rel=0';
-    var vimeo = 'https://player.vimeo.com/video/265899201';
-    playerFrame.attr('src', vimeo);
-    try {
-        var image = new Image();
-        image.onload = function () {
-            playerFrame.attr('src', youTube);
-        };
-        image.onerror = function () {
-            var image2 = new Image();
-            image2.onload = function () {
-                playerFrame.attr('src', youTubeNoCookie);
-            };
-            image2.onerror = function () {
-                playerFrame.attr('src', vimeo);
-            };
-            image2.src = "https://www.youtube-nocookie.com/favicon.ico";
-        };
-        image.src = "https://youtube.com/favicon.ico";
-    } catch (error) {
-        playerFrame.attr('src', vimeo);
-    }
+  var playerFrame = $("#ytplayer");
+  var youTube = "https://www.youtube.com/embed/KmaOW4PhuII?rel=0";
+  var youTubeNoCookie =
+    "https://www.youtube-nocookie.com/embed/KmaOW4PhuII?rel=0";
+  var vimeo = "https://player.vimeo.com/video/265899201";
+  playerFrame.attr("src", vimeo);
+  try {
+    var image = new Image();
+    image.onload = function () {
+      playerFrame.attr("src", youTube);
+    };
+    image.onerror = function () {
+      var image2 = new Image();
+      image2.onload = function () {
+        playerFrame.attr("src", youTubeNoCookie);
+      };
+      image2.onerror = function () {
+        playerFrame.attr("src", vimeo);
+      };
+      image2.src = "https://www.youtube-nocookie.com/favicon.ico";
+    };
+    image.src = "https://youtube.com/favicon.ico";
+  } catch (error) {
+    playerFrame.attr("src", vimeo);
+  }
 }
